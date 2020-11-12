12fe7c3ff02eb5be930fd2775ace66bf70feffa3
dkapkap <dhruv.kapoor@trader.ca>
2020-11-11 16:06:22 -0800

    clone unet and call it nnret for now

---
+++ fastmri/pl_modules/nnret_module.py
@@ -0,0 +1,152 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+from argparse import ArgumentParser
+
+import torch
+from fastmri.models import NnRet
+from torch.nn import functional as F
+
+from .mri_module import MriModule
+
+
+class NnRetModule(MriModule):
+    def __init__(
+        self,
+        in_chans=1,
+        out_chans=1,
+        chans=32,
+        num_pool_layers=4,
+        drop_prob=0.0,
+        lr=0.001,
+        lr_step_size=40,
+        lr_gamma=0.1,
+        weight_decay=0.0,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.save_hyperparameters()
+
+        self.in_chans = in_chans
+        self.out_chans = out_chans
+        self.chans = chans
+        self.num_pool_layers = num_pool_layers
+        self.drop_prob = drop_prob
+        self.lr = lr
+        self.lr_step_size = lr_step_size
+        self.lr_gamma = lr_gamma
+        self.weight_decay = weight_decay
+
+        self.nnret = NnRet(
+            in_chans=self.in_chans,
+            out_chans=self.out_chans,
+            chans=self.chans,
+            num_pool_layers=self.num_pool_layers,
+            drop_prob=self.drop_prob,
+        )
+
+    def forward(self, image):
+        return self.nnret(image.unsqueeze(1)).squeeze(1)
+
+    def training_step(self, batch, batch_idx):
+        image, target, _, _, _, _, _ = batch
+        output = self(image)
+        loss = F.l1_loss(output, target)
+
+        self.log("loss", loss.detach())
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        image, target, mean, std, fname, slice_num, max_value = batch
+        output = self(image)
+        mean = mean.unsqueeze(1).unsqueeze(2)
+        std = std.unsqueeze(1).unsqueeze(2)
+
+        return {
+            "batch_idx": batch_idx,
+            "fname": fname,
+            "slice_num": slice_num,
+            "max_value": max_value,
+            "output": output * std + mean,
+            "target": target * std + mean,
+            "val_loss": F.l1_loss(output, target),
+        }
+
+    def test_step(self, batch, batch_idx):
+        image, _, mean, std, fname, slice_num, _ = batch
+        output = self.forward(image)
+        mean = mean.unsqueeze(1).unsqueeze(2)
+        std = std.unsqueeze(1).unsqueeze(2)
+
+        return {
+            "fname": fname,
+            "slice": slice_num,
+            "output": (output * std + mean).cpu().numpy(),
+        }
+
+    def configure_optimizers(self):
+        optim = torch.optim.RMSprop(
+            self.parameters(),
+            lr=self.lr,
+            weight_decay=self.weight_decay,
+        )
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optim, self.lr_step_size, self.lr_gamma
+        )
+
+        return [optim], [scheduler]
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):  # pragma: no-cover
+        """
+        Define parameters that only apply to this model
+        """
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser = MriModule.add_model_specific_args(parser)
+
+        # network params
+        parser.add_argument(
+            "--in_chans", default=1, type=int, help="Number of NNRET input channels"
+        )
+        parser.add_argument(
+            "--out_chans", default=1, type=int, help="Number of NNRET output chanenls"
+        )
+        parser.add_argument(
+            "--chans", default=1, type=int, help="Number of top-level NNRET filters."
+        )
+        parser.add_argument(
+            "--num_pool_layers",
+            default=4,
+            type=int,
+            help="Number of NNRET pooling layers.",
+        )
+        parser.add_argument(
+            "--drop_prob", default=0.0, type=float, help="NNRET dropout probability"
+        )
+
+        # training params (opt)
+        parser.add_argument(
+            "--lr", default=0.001, type=float, help="RMSProp learning rate"
+        )
+        parser.add_argument(
+            "--lr_step_size",
+            default=40,
+            type=int,
+            help="Epoch at which to decrease step size",
+        )
+        parser.add_argument(
+            "--lr_gamma", default=0.1, type=float, help="Amount to decrease step size"
+        )
+        parser.add_argument(
+            "--weight_decay",
+            default=0.0,
+            type=float,
+            help="Strength of weight decay regularization",
+        )
+
+        return parser
