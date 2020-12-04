732b1ae4e4625e6b7690e9f0495e9c05cc242fce
Venkat Tarakad <ven7782@gmail.com>
2020-12-03 22:41:15 -0500

    Fix error on train_resnet_demo

--- experimental/resnet/train_resnet_demo.py
+++ experimental/resnet/train_resnet_demo.py
@@ -13,7 +13,7 @@ from argparse import ArgumentParser
 import pytorch_lightning as pl
 from fastmri.data.mri_data import fetch_dir
 from fastmri.data.subsample import create_mask_for_mask_type
-from fastmri.data.transforms import ResNetDataTransform, UnetDataTransform
+from fastmri.data.transforms import UnetDataTransform
 from fastmri.pl_modules import FastMriDataModule, ResNetModule
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
